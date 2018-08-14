3bb3b3af3e1a49bbb49104b4f85374c4148c274b
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-08-14 21:38:48 +0900

    Delete a wrong spec

--- spec/ec2spec_spec.rb
+++ spec/ec2spec_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ec2spec do
   it 'has a version number' do
     expect(Ec2spec::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
