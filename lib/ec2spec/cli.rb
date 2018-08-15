faff22504f6b865cab8d336ea9c1e11ea33447aa
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-08-15 16:46:24 +0900

    Add debug log option

--- lib/ec2spec/cli.rb
+++ lib/ec2spec/cli.rb
@@ -7,13 +7,19 @@ module Ec2spec
     option 'days', type: :numeric
     option 'format'
     option 'region'
+    option 'debug', type: :boolean
+
+    # rubocop:disable Metrics/AbcSize
     def ssh
       hosts = options['host']
       days = options['days']
       format = options['format'] || :plain_text
       region = options['region'] || 'ap-northeast-1'
+
+      Ec2spec.logger.level = Logger::DEBUG if options['debug']
       client = Ec2spec::Client.new(hosts, days, format, region)
       puts client.run
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
