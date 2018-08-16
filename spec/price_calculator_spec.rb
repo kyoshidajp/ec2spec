bfd26ed3aecc44bcac858c9b3f609fa82d1ba51f
Katsuhiko YOSHIDA <claddvd@gmail.com>
2018-08-16 22:05:50 +0900

    Fix 'No backend type' error when running rspec

--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Ec2spec::PriceCalculator do
   describe '.prepare' do
     it 'accepts api calc type' do
-      expect(Ec2spec::PriceCalculator.instance.prepare('JPY', 110, 'api'))
+      expect(Ec2spec::PriceCalculator.instance.prepare('JPY', 110, 'api', 'app-id'))
         .to be_an_instance_of Ec2spec::PriceCalculator
     end
     it 'accepts manual calc type' do
@@ -25,19 +25,19 @@ RSpec.describe Ec2spec::PriceCalculator do
   describe '.currency_values?' do
     subject { Ec2spec::PriceCalculator.instance.currency_values? }
     context 'having both @currency_unit and @dollar_exchange_rate' do
-      before { Ec2spec::PriceCalculator.instance.prepare('JPY', 110, 'api') }
+      before { Ec2spec::PriceCalculator.instance.prepare('JPY', 110, 'manual') }
       it { is_expected.to be_truthy }
     end
     context 'having only @currency_unit' do
-      before { Ec2spec::PriceCalculator.instance.prepare('JPY', nil, 'api') }
+      before { Ec2spec::PriceCalculator.instance.prepare('JPY', nil, 'manual') }
       it { is_expected.to be_falsey }
     end
     context 'having only @dollar_exchange_rate' do
-      before { Ec2spec::PriceCalculator.instance.prepare(nil, 110, 'api') }
+      before { Ec2spec::PriceCalculator.instance.prepare(nil, 110, 'manual') }
       it { is_expected.to be_falsey }
     end
     context 'having neither @currency_unit nor @dollar_exchange_rate' do
-      before { Ec2spec::PriceCalculator.instance.prepare(nil, nil, 'api') }
+      before { Ec2spec::PriceCalculator.instance.prepare(nil, nil, 'manual') }
       it { is_expected.to be_falsey }
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,3 +13,5 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+Specinfra.configuration.backend = :exec
